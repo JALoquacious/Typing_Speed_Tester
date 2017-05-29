3d4cb3c9aa0d379aea9feaf487365bab19e87355
JALoquacious <jalikoudis@protonmail.com>
2017-05-29 15:00:26 -0400

    Completed working version

--- js/logic.js
+++ js/logic.js
@@ -1,38 +1,57 @@
-const originText  = document.querySelector('#origin-text'),
-      resetButton = document.querySelector('#reset'),
-      timeClock   = document.querySelector('#timer'),
-      typedText   = document.querySelector('#typed-text');
+const originTextArea  = document.querySelector('#origin-text'),
+      resetButton     = document.querySelector('#reset'),
+      timeClock       = document.querySelector('#timer'),
+      typedTextArea   = document.querySelector('#typed-text');
 
-let timer = 0;
+let timer    = 0,
+    active   = false,
+    interval = null;
 
 function initTest() {
-    let enteredText = typedText.value;
-    if (enteredText.length === 0) {
-        window.setInterval(count, 10);
+    if (typedTextArea.value.length === 0 && !active) {
+        active = true;
+        interval = window.setInterval(count, 10);
     }
 }
 
-function compareOrigin() {
-    //
+function compareTypedToOrigin() {
+    let typedText     = typedTextArea.value,
+        originText    = originTextArea.value,
+        originSegment = originTextArea.value.substring(0, typedText.length);
+    
+    if (typedText === originText) {
+        window.clearInterval(interval);
+        typedTextArea.style.borderColor = '#8fbc8f';
+    } else if (typedText === originSegment) {
+        typedTextArea.style.borderColor = '#8f94bc';
+    } else {
+        typedTextArea.style.borderColor = '#ffa07d';
+    }
 }
 
-function count() {
-    let minutes = Math.floor(timer / 100 / 60),
-        seconds = Math.floor(timer / 100) - (minutes * 60),
+function count(forward = true) {
+    let minutes    = Math.floor(timer / 100 / 60),
+        seconds    = Math.floor(timer / 100) - (minutes * 60),
         hundredths = timer - (seconds * 100) - (minutes * 60 * 100);
     
     [minutes, seconds, hundredths] = [minutes, seconds, hundredths].map(
         t => (t < 10) ? `0${t}` : t
     );
     
-    timeClock.innerHTML = `${minutes}:${seconds}:${hundredths}`;
-    timer++;
+    timeClock.textContent = `${minutes}:${seconds}:${hundredths}`;
+    if (forward) timer++;
 }
 
 function reset() {
+    window.clearInterval(interval);
     timer = 0;
+    active = false;
+    count(forward = false);
+    originTextArea.value = '';
+    typedTextArea.value = '';
+    typedTextArea.style.borderColor = '#888888';
 }
 
 resetButton.addEventListener('click', reset);
-typedText.addEventListener('keypress', initTest);
-typedText.addEventListener('keyup', compareOrigin);
+typedTextArea.addEventListener('keypress', initTest);
+typedTextArea.addEventListener('keyup', compareTypedToOrigin);
